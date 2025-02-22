89676c7d629d13edd82ff082ace050064e2d93e9
Nhmives <nhmovies9@gmail.com>
2025-02-22 10:51:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,6 +1,6 @@
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Ask Doubt on telegram @insightweav
 
 # Never Give DATABASE_URI And BOT_TOKEN Here If Repo Is Forked Or Public Because This May Lead Account Ban.
 
@@ -13,4 +13,4 @@ BOT_TOKEN    = environ.get("BOT_TOKEN", "")
 DATABASE_URI = environ.get("DATABASE_URI", "")
 LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002047453577"))
 ADMIN        = int(environ.get("ADMIN", "6276113288"))
-CHANNEL      = environ.get("CHANNEL", "@VJ_Botz")
+CHANNEL      = environ.get("CHANNEL", "@new_updet")

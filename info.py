c6475f6daa808213f5da24126cb27d13996df9ac
mdnazbul <mdnazbul1@gmail.com>
2025-02-22 22:45:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -9,8 +9,8 @@ from os import environ
 
 API_ID       = int(environ.get("API_ID", "20496814"))
 API_HASH     = environ.get("API_HASH", "a87c1094edd18650e5dfee0f2bc78bda")
-BOT_TOKEN    = environ.get("BOT_TOKEN", "")
-DATABASE_URI = environ.get("DATABASE_URI", "")
+BOT_TOKEN    = environ.get("BOT_TOKEN", "7280713878:AAHOw5TcAjQIAholI1jLQ2V45A44wnohcsU")
+DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://nhmovies9:nhmovies9@cluster0.lp65c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002047453577"))
 ADMIN        = int(environ.get("ADMIN", "6276113288"))
 CHANNEL      = environ.get("CHANNEL", "@new_updet")
